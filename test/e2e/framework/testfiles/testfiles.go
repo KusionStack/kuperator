a006d91817b762fa27ff43d3e1e0718ccb105a71
Chaer <49875152+Eikykun@users.noreply.github.com>
2023-08-01 11:28:36 +0800

    init ci.yaml (#14)
    * init ci.yaml

--- test/e2e/framework/testfiles/testfiles.go
+++ test/e2e/framework/testfiles/testfiles.go
@@ -21,7 +21,6 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
-	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
@@ -114,7 +113,7 @@ func (r RootFileSource) ReadTestFile(filePath string) ([]byte, error) {
 	} else {
 		fullPath = filepath.Join(r.Root, filePath)
 	}
-	data, err := ioutil.ReadFile(fullPath)
+	data, err := os.ReadFile(fullPath)
 	if os.IsNotExist(err) {
 		// Not an error (yet), some other provider may have the file.
 		return nil, nil
