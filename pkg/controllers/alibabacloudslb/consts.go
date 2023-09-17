603ac6d3225ab8624bbbc66455d4decc0ab34d29
WeichengWang1 <124144418+WeichengWang1@users.noreply.github.com>
2023-09-17 19:54:35 +0800

    rename alibaba_cloud_slb to alibabacloudslb (#86)

--- pkg/controllers/alibabacloudslb/consts.go
+++ pkg/controllers/alibabacloudslb/consts.go
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package alibaba_cloud_slb
+package alibabacloudslb
 
 const alibabaCloudSlbLbIdLabelKey = "service.k8s.alibaba/loadbalancer-id"
