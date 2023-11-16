c3571f2dc943f4812565ce6f7c8a62adf2e89671
wu8685 <optimuswu8685@gmail.com>
2023-11-16 11:05:27 +0800

    test: add some tests for expectation

---
+++ pkg/webhook/server/generic/collaset/collaset_mutating_handler_test.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2023 The KusionStack Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package collaset
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1alpha1 "kusionstack.io/operating/apis/apps/v1alpha1"
+)
+
+func TestMutatingCollaSet(t *testing.T) {
+	cls := &appsv1alpha1.CollaSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Spec: appsv1alpha1.CollaSetSpec{},
+	}
+
+	appsv1alpha1.SetDetaultCollaSet(cls)
+
+	if cls.Spec.UpdateStrategy.PodUpdatePolicy != appsv1alpha1.CollaSetInPlaceIfPossiblePodUpdateStrategyType {
+		t.Fatalf("expected default value is %s, got %s", appsv1alpha1.CollaSetInPlaceIfPossiblePodUpdateStrategyType,
+			cls.Spec.UpdateStrategy.PodUpdatePolicy)
+	}
+
+	if cls.Spec.UpdateStrategy.RollingUpdate == nil {
+		t.Fatalf("expected default rollingUpdate, got nil")
+	}
+
+	if cls.Spec.UpdateStrategy.RollingUpdate.ByPartition == nil {
+		t.Fatalf("expected default byPartition, got nil")
+	}
+}
