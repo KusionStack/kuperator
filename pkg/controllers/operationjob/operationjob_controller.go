2530ef93087bc311b6563ec355266d645bd78e09
ColdsteelRail <574252631@qq.com>
2024-07-29 15:58:56 +0800

    oj shoule not be reconciled if failed or succeeded

--- pkg/controllers/operationjob/operationjob_controller.go
+++ pkg/controllers/operationjob/operationjob_controller.go
@@ -127,8 +127,10 @@ func (r *ReconcileOperationJob) Reconcile(ctx context.Context, req reconcile.Req
 		return reconcile.Result{}, err
 	}
 
-	if err := r.doReconcile(ctx, instance, logger); err != nil {
-		return reconcile.Result{}, err
+	if !ojutils.IsJobFinished(instance) {
+		if err := r.doReconcile(ctx, instance, logger); err != nil {
+			return reconcile.Result{}, err
+		}
 	}
 
 	jobDeleted, requeueAfter, err := r.ensureActiveDeadlineAndTTL(ctx, instance, logger)
