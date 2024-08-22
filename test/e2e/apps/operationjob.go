a523b0ab083b19ac7681a136f5c0dd80d0ffc487
ColdsteelRail <574252631@qq.com>
2024-08-22 15:05:59 +0800

    fix go lint

--- test/e2e/apps/operationjob.go
+++ test/e2e/apps/operationjob.go
@@ -27,6 +27,7 @@ import (
 	appsv1alpha1 "kusionstack.io/kube-api/apps/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"kusionstack.io/kuperator/pkg/controllers/operationjob/replace"
 	"kusionstack.io/kuperator/test/e2e/framework"
 )
 
@@ -132,7 +133,7 @@ var _ = SIGDescribe("OperationJob", func() {
 			Eventually(func() error { return ojTester.ExpectOperationJobProgress(oj, appsv1alpha1.OperationProgressFailed) }, 30*time.Second, 3*time.Second).ShouldNot(HaveOccurred())
 
 			By("Check reason for Failed")
-			Expect(oj.Status.TargetDetails[0].Reason).To(Equal(appsv1alpha1.ReasonPodNotFound))
+			Expect(oj.Status.TargetDetails[0].Error.Reason).To(Equal(appsv1alpha1.ReasonPodNotFound))
 		})
 
 		framework.ConformanceIt("operationjob replace pod parallel", func() {
@@ -164,7 +165,7 @@ var _ = SIGDescribe("OperationJob", func() {
 			Eventually(func() error { return ojTester.ExpectOperationJobProgress(oj2, appsv1alpha1.OperationProgressSucceeded) }, 30*time.Second, 3*time.Second).ShouldNot(HaveOccurred())
 
 			By("Check replace new pod of OperationJobs")
-			Expect(oj1.Status.TargetDetails[0].Message).To(Equal(oj2.Status.TargetDetails[0].Message))
+			Expect(oj1.Status.TargetDetails[0].ExtraInfo[replace.ExtraInfoReplacedByNewPodKey]).To(Equal(oj2.Status.TargetDetails[0].ExtraInfo[replace.ExtraInfoReplacedByNewPodKey]))
 		})
 
 		framework.ConformanceIt("delete operationjob to cancel replace", func() {
