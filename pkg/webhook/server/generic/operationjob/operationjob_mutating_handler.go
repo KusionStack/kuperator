4df7763f01c886e20ca81e1278a095c0f5b5b6ee
hexin <574252631@qq.com>
2024-11-13 14:19:00 +0800

    Feat: Set default DL adn TTL for opj (#303)
    * set default dl adn ttl for opj
    
    * fix golint

--- pkg/webhook/server/generic/operationjob/operationjob_mutating_handler.go
+++ pkg/webhook/server/generic/operationjob/operationjob_mutating_handler.go
@@ -18,7 +18,13 @@ package operationjob
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	admissionv1 "k8s.io/api/admission/v1"
+	"k8s.io/utils/ptr"
+	appsv1alpha1 "kusionstack.io/kube-api/apps/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
@@ -27,6 +33,11 @@ import (
 
 var _ inject.Client = &MutatingHandler{}
 var _ admission.DecoderInjector = &MutatingHandler{}
+
+var (
+	defaultTTL            int32 = 30 * 60
+	defaultActiveDeadline int32 = 3 * 60 * 60
+)
 
 type MutatingHandler struct {
 	*mixin.WebhookHandlerMixin
@@ -39,5 +50,27 @@ func NewMutatingHandler() *MutatingHandler {
 }
 
 func (h *MutatingHandler) Handle(ctx context.Context, req admission.Request) (resp admission.Response) {
-	return admission.Allowed("")
+	if req.Operation != admissionv1.Update && req.Operation != admissionv1.Create {
+		return admission.Allowed("")
+	}
+	opj := &appsv1alpha1.OperationJob{}
+	if err := h.Decoder.Decode(req, opj); err != nil {
+		return admission.Errored(http.StatusBadRequest, fmt.Errorf("failed to decode OperationJob: %s", err))
+	}
+
+	SetDefaultSpec(opj)
+	marshalled, err := json.Marshal(opj)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+	return admission.PatchResponseFromRaw(req.AdmissionRequest.Object.Raw, marshalled)
+}
+
+func SetDefaultSpec(opj *appsv1alpha1.OperationJob) {
+	if opj.Spec.ActiveDeadlineSeconds == nil {
+		opj.Spec.ActiveDeadlineSeconds = ptr.To(defaultActiveDeadline)
+	}
+	if opj.Spec.TTLSecondsAfterFinished == nil {
+		opj.Spec.TTLSecondsAfterFinished = ptr.To(defaultTTL)
+	}
 }
