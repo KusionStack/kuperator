d21ef2b4cd30d5c9bbedb9b398b5df32a68f3bcb
ColdsteelRail <574252631@qq.com>
2025-04-24 22:26:21 +0800

    feat: support allow privileged

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
+	"k8s.io/kubernetes/pkg/capabilities"
 	appsv1alpha1 "kusionstack.io/kube-api/apps/v1alpha1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
@@ -58,9 +59,12 @@ func main() {
 		probeAddr            string
 		certDir              string
 		dnsName              string
+		allowPrivileged      bool
 	)
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&allowPrivileged, "allow-privileged", true, "If true, allow privileged containers. It will only work if api-server is also"+
+		"started with --allow-privileged=true.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -69,6 +73,12 @@ func main() {
 
 	klog.InitFlags(nil)
 	defer klog.Flush()
+
+	if allowPrivileged {
+		capabilities.Initialize(capabilities.Capabilities{
+			AllowPrivileged: allowPrivileged,
+		})
+	}
 
 	feature.DefaultMutableFeatureGate.AddFlag(pflag.CommandLine)
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
