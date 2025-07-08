4639c93fa3875599a29ea237b0be46a31dd827f8
ColdsteelRail <574252631@qq.com>
2025-07-08 16:15:14 +0800

    improve: release update when not in partition

--- pkg/controllers/collaset/collaset_controller_test.go
+++ pkg/controllers/collaset/collaset_controller_test.go
@@ -2382,7 +2382,7 @@ var _ = Describe("collaset controller", func() {
 		Eventually(func() error {
 			// check updated pod replicas by CollaSet status
 			return expectedStatusReplicas(c, cs, 0, 0, 0, 2, 2, 0, 0, 0)
-		}, 300*time.Second, 1*time.Second).Should(BeNil())
+		}, 30*time.Second, 1*time.Second).Should(BeNil())
 	})
 
 	It("replace update change to inplaceUpdate", func() {
